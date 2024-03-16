abffe053fbdff8e3f192910041eb9a04d678c963
AxelothLeohryn <alejandro97ss@gmail.com>
2024-03-16 11:37:45 +0100

    fix mobile 3d model pan

--- src/components/VideoCarousel.jsx
+++ src/components/VideoCarousel.jsx
@@ -197,7 +197,7 @@ const VideoCarousel = () => {
         <div className="flex-center py-5 px-7 bg-gray-300 backdrop-blur rounded-full">
           {videoRef.current.map((_, index) => (
             <span
-              className="relative mx-2 w-3 h-3 bg-gray-200 rounded-full cursor-pointer"
+              className="relative mx-2 w-3 h-3 bg-gray-200 rounded-full cursor-pointer overflow-hidden"
               key={index}
               ref={(el) => (videoDivRef.current[index] = el)}
               //add onClick here
